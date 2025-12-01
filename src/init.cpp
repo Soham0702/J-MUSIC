27505efe77c742903172f0bd6a5eafc1bb3e19eb
Soham <hisohambanerjee@gmail.com>
2025-12-02 01:14:02 +0530

    fixed neta and deta

--- src/init.cpp
+++ src/init.cpp
@@ -109,12 +109,10 @@ void Init::InitArena(SCGrid &arena_prev, SCGrid &arena_current,
                       << ". Overwriting lattice dimensions:";
         DATA.nx = nx;
         DATA.ny = ny;
-        DATA.neta = neta;
         DATA.delta_x = dx;
         DATA.delta_y = dy;
-        DATA.delta_eta = 0.1;
 
-        music_message << "neta=" << neta << ", nx=" << nx << ", ny=" << ny;
+        music_message << "neta=" << DATA.neta << ", nx=" << nx << ", ny=" << ny;
         music_message << "deta=" << DATA.delta_eta << ", dx=" << DATA.delta_x
                       << ", dy=" << DATA.delta_y;
         music_message.flush("info");
